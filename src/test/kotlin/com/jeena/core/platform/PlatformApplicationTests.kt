2476b2753826a56585fd48a042ce0d099599e5fc
support <support@astrobasic.com>
2023-02-19 21:08:34 -0800

    first line

---
+++ src/test/kotlin/com/jeena/core/platform/PlatformApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jeena.core.platform
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PlatformApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
